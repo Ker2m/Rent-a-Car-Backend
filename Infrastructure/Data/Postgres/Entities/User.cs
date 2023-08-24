89ba62404c40daa2f211fd7c0b60001bad98d387
Kerim <Kerim5781@gmail.com>
2023-08-24 22:22:09 +0300

    Rent-a Car Backend
    3 kişilik ekibimle .Net ve Postgresql kullanarak yaptığımız rent-a car projesi.

---
+++ Infrastructure/Data/Postgres/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Data.Postgres.Entities.Base;
+
+namespace Infrastructure.Data.Postgres.Entities;
+
+public class User : Entity<int>
+{
+    public string tc_no { get; set; } = default!;
+    public string UserName { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string phone { get; set; } = default!;
+    public byte[] PasswordSalt { get; set; } = default!;
+    public byte[] PasswordHash { get; set; } = default!;
+    public UserType UserType { get; set; }
+    public ICollection<Comment> Comments { get; set; }
+    public ICollection<Reservations> Reservations { get; set; }
+}
+
+//denemecadas
+//rent-a-car-backend isim denemesi (repository ismini github üzerinden değiştirdim)
+
+public enum UserType
+{
+    Admin,
+    User,
+}
